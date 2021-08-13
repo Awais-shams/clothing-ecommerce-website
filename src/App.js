63b82290f4ac580c46e6aaec39ff7c2286413f58
Awais Shams <awaisshams@Awaiss-MacBook-Pro.local>
2021-08-13 16:51:31 +0500

    Updated the project with navigation and routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { Switch,Route } from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage';
+import HatsMenu from './components/directory-menu/hats-menu/HatsMenu';
+import React from 'react'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+    <Switch>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/hats' component={HatsMenu}/>
+    </Switch>
+    
+   
     </div>
+    
+
   );
 }
 
---
+++ src/components/directory-menu/hats-menu/HatsMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HatsMenu = () => {
+    return (
+        <div>
+            <h1>Hats Page</h1>
+        </div>
+    )
+}
+
+export default HatsMenu;
